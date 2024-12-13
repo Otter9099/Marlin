4ef26df52a6486bb06826565c1202e004ab9e9aa
ellensp <530024+ellensp@users.noreply.github.com>
2024-12-13 14:19:12 +1300

    update SMOOTHIEBOARD comment

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -497,7 +497,7 @@
 #elif MB(COHESION3D_MINI)
   #include "lpc1769/pins_COHESION3D_MINI.h"         // LPC1769                              env:LPC1769
 #elif MB(SMOOTHIEBOARD)
-  #include "lpc1769/pins_SMOOTHIEBOARD.h"           // LPC1769                              env:LPC1768 env:LPC1769
+  #include "lpc1769/pins_SMOOTHIEBOARD.h"           // LPC1768/9                            env:LPC1768 env:LPC1769
 #elif MB(TH3D_EZBOARD)
   #include "lpc1769/pins_TH3D_EZBOARD.h"            // LPC1769                              env:LPC1769
 #elif MB(BTT_SKR_V1_4_TURBO)
