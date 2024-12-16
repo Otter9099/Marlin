f01d76a3413b0b29b0e861e172902878a6b8b7ce
Switchleg1 <89221799+Switchleg1@users.noreply.github.com>
2024-12-16 14:06:07 -0700

    Adds acceleration_rate in planner.h when S_CURVE acceleration is being used.

--- Marlin/src/module/planner.h
+++ Marlin/src/module/planner.h
@@ -246,6 +246,9 @@ typedef struct PlannerBlock {
              deceleration_time,
              acceleration_time_inverse,     // Inverse of acceleration and deceleration periods, expressed as integer. Scale depends on CPU being used
              deceleration_time_inverse;
+  #if ENABLED(FREEZE_FEATURE)
+    uint32_t acceleration_rate;             // The acceleration rate used for acceleration calculation
+  #endif
   #else
     uint32_t acceleration_rate;             // Acceleration rate in (2^24 steps)/timer_ticks*s
   #endif
